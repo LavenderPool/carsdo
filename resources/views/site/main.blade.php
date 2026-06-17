a8ca4f329aee164577fbf444b0817db345124fbc
Sergey <sekret200509@gmail.com>
2026-06-17 19:23:53 +0300

    upd

--- resources/views/site/main.blade.php
+++ resources/views/site/main.blade.php
@@ -53,9 +53,31 @@
     <div class="homecrash">
     <ul class="ctc">
     @foreach ($crashTests as $crashTest)
+        @php
+            $car = $crashTest->car;
+            $cardBrand = $car?->brand;
+            $ratingValue = is_numeric($crashTest->rating ?? null) ? (int) $crashTest->rating : null;
+            $ratingStars = $ratingValue && $ratingValue > 0 ? max(1, min(5, $ratingValue)) : null;
+        @endphp
+        @continue(!$car || !$cardBrand)
         <li>
-            <a href="/{{ $crashTest->car->brand->slug }}/{{ $crashTest->car->slug }}/crash-test/">
-                <img alt="Краш-тест {{ $crashTest->car->brand->name }} {{ $crashTest->car->name }}" src="{{ $crashTest->car->coverUrl() }}">
+            <a href="/{{ $cardBrand->slug }}/{{ $car->slug }}/crash-test/">
+                <span class="crash-test-card">
+                    <img alt="Краш-тест {{ $cardBrand->name }} {{ $car->name }}" src="{{ $car->coverUrl() }}">
+                    <span class="crash-test-card__overlay"></span>
+                    <span class="crash-test-card__content">
+                        <span class="crash-test-card__title">{{ $car->name }}</span>
+                        @if($ratingStars)
+                            <span class="crash-test-card__stars" aria-label="Рейтинг {{ $ratingStars }} из 5">
+                                @foreach(range(1, $ratingStars) as $star)
+                                    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                                    </svg>
+                                @endforeach
+                            </span>
+                        @endif
+                    </span>
+                </span>
             </a>
         </li>
     @endforeach
@@ -72,9 +94,18 @@
     <div class="homecrash">
     <ul class="ctc">
     @foreach ($testDrives as $testDrive)
+        @php
+            $car = $testDrive->car;
+            $cardBrand = $car?->brand;
+        @endphp
+        @continue(!$car || !$cardBrand)
         <li>
-            <a href="/{{ $testDrive->car->brand->slug }}/{{ $testDrive->car->slug }}/test-drive/">
-                <img alt="Тест-драйв {{ $testDrive->car->brand->name }} {{ $testDrive->car->name }}" src="{{ $testDrive->car->coverUrl() }}">
+            <a href="/{{ $cardBrand->slug }}/{{ $car->slug }}/test-drive/">
+                <span class="test-drive-card">
+                    <img alt="Тест-драйв {{ $cardBrand->name }} {{ $car->name }}" src="{{ $car->coverUrl() }}">
+                    <span class="test-drive-card__overlay"></span>
+                    <span class="test-drive-card__title">{{ $car->name }}</span>
+                </span>
             </a>
         </li>
     @endforeach
